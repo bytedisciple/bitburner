3407f0b18a89ff960b2a673f7c4fded637083f7b
bytedisciple <46268272+bytedisciple@users.noreply.github.com>
2019-08-01 00:23:54 -0400

    Update and rename grow.js to grow.script

--- worm/grow.js
+++
@@ -1,5 +0,0 @@
-hostname=args[0];
-while(getServerMoneyAvailable(hostname) < getServerMaxMoney(hostname) * 0.65){
-    grow(hostname, {threads : args[2]});
-}
-spawn("weaken.script", args[3], args[0], args[1], args[2], args[3])
