3a3227e9760696c2904f9e68e139db7d03740de6
bytedisciple <46268272+bytedisciple@users.noreply.github.com>
2019-08-01 00:25:09 -0400

    Update and rename weaken.js to weaken.script

--- worm/weaken.js
+++
@@ -1,5 +0,0 @@
-hostname = args[0];
-while(getServerSecurityLevel(hostname) > getServerBaseSecurityLevel(hostname)){
-    weaken(hostname, {threads : args[3]});
-}
-spawn("hack.script", args[1], args[0], args[1], args[2], args[3])
